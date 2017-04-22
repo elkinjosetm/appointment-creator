b9e51aa2055c95127f1e76b5a8367a4dfde08e63
Elkin Torres <elkinjosetm@gmail.com>
2017-04-22 12:08:51 -0500

    Show/hide calendar behavior

--- src/js/datepicker.js
+++ src/js/datepicker.js
@@ -12,7 +12,11 @@
 		datePickers.forEach( datePicker => {
 			// Build picker layout
 			buildPickerLayout( datePicker );
+			addEventHandler( datePicker );
 		} );
+
+		// Handle click out event
+		document.addEventListener( 'click', blurHandler );
 	}
 
 	const getElementsByClass = ( className, sourceElement = document ) => {
@@ -25,6 +29,36 @@
 		// Add calendar container below the input
 		calendar.className = classNames.calendar;
 		datePicker.appendChild( calendar );
+	}
+
+	const addEventHandler = datePicker => {
+		const input = getElementsByClass( `.${ classNames.input }`, datePicker )[ 0 ];
+
+		input.addEventListener( 'click', event => {
+			closePickers();
+
+			if ( ! datePicker.classList.contains( classNames.active ) )
+				datePicker.classList.add( classNames.active );
+		} );
+	}
+
+	const closePickers = () => {
+		const activePickers = getElementsByClass( `.${ classNames.wrapper }.${ classNames.active }` );
+
+		activePickers.forEach( datePicker => {
+			datePicker.classList.remove( classNames.active );
+		} );
+	}
+
+	const blurHandler = event => {
+		const isInput    = event.target.classList.contains( classNames.input );
+		const isCalendar = event.target.classList.contains( classNames.calendar );
+
+		// Prevent close pickers if user clicked one of the pickers
+		if ( isInput || isCalendar )
+			return;
+
+		closePickers();
 	}
 
 	init();
